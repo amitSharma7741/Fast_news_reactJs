0471c3192503b11a18eb1e4b300a38bafbae2051
amitSharma7741 <183099@nith.ac.in>
2022-08-19 00:19:09 +0530

    adding links

--- src/Components/LeftSideBar.js
+++ src/Components/LeftSideBar.js
@@ -7,7 +7,7 @@ import { FaFortAwesome } from 'react-icons/fa';
 import { FaTeamspeak } from 'react-icons/fa';
 import { FaArchive } from 'react-icons/fa';
 import { FaBabyCarriage } from 'react-icons/fa';
-
+import { Outlet, Link } from "react-router-dom";
 const LeftSideBar = () => {
   const isM_Or_L = useMediaQuery({
     query: '(min-width: 700px)'
@@ -19,32 +19,33 @@ const LeftSideBar = () => {
            width: isM_Or_L ? "150px" :"100%"}}>
           <ul className="nav nav-pills flex-sm-column flex-row mb-auto justify-content-between text-truncate">
             <li className="nav-item">
-              <a href="/" className="nav-link px-2 text-truncate">
+              <Link to="/" className="nav-link px-2 text-truncate">
               <FaHome aria-hidden="true" />
                 <span className="d-none d-sm-inline">Home</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/" className="nav-link px-2 text-truncate">
+              <Link to="/about" className="nav-link px-2 text-truncate">
               <FaFortAwesome aria-hidden="true" />
-                <span className="d-none d-sm-inline">Dashboard</span>
-              </a>
+                <span className="d-none d-sm-inline">World</span>
+              </Link>
             </li>
             <li>
-              <a href="/" className="nav-link px-2 text-truncate"><FaTeamspeak aria-hidden="true" />
-                <span className="d-none d-sm-inline">Orders</span> </a>
+              <Link to="/category" className="nav-link px-2 text-truncate"><FaTeamspeak aria-hidden="true" />
+                <span className="d-none d-sm-inline">Orders</span> </Link>
             </li>
             <li>
-              <a href="/" className="nav-link px-2 text-truncate"><FaArchive aria-hidden="true" />
-                <span className="d-none d-sm-inline">Products</span> </a>
+              <Link to="/about" className="nav-link px-2 text-truncate"><FaArchive aria-hidden="true" />
+                <span className="d-none d-sm-inline">Products</span> </Link>
             </li>
             <li>
-              <a href="/" className="nav-link px-2 text-truncate"><FaBabyCarriage aria-hidden="true" />
-                <span className="d-none d-sm-inline">Customers</span> </a>
+              <Link to="/category" className="nav-link px-2 text-truncate"><FaBabyCarriage aria-hidden="true" />
+                <span className="d-none d-sm-inline">Customers</span> </Link>
             </li>
           </ul>
         </div>
       </aside> 
+      <Outlet />
     </>
   )
 }
