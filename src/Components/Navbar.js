4628748d27588e9787006b828595077caf6d6c1a
amitSharma7741 <183099@nith.ac.in>
2022-08-27 00:32:12 +0530

    ..

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -25,14 +25,14 @@ const Navbar = () => {
 
         <div className="price-container">
           <div className="price-box">
-            {dim.map((ite) => (
+            {dim.map((ite ) => (
               <>
-                <div className="box-styling">
+                <div className="box-styling" key = {ite.rank}>
 
                   <span className="text-uppercase d-flex" style={{ textAlign: "justify" }}> {ite.symbol} </span>
                   <span className="price-span"> {ite.priceUsd.slice(0, 8)} </span>
 
-                  <span className="up-down">
+                  <span className="up-down"> 
                   {ite.changePercent24Hr.slice(0, 1) === "-"  ? <FaAngleDown style={{ color: "red"}}/> : <FaAngleUp style={{ color: "green" }} />}
                   </span>
                   <b>
