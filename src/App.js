72109964076c49fbb620533aeefb1ecde0ac87ae
amitSharma7741 <183099@nith.ac.in>
2022-08-27 00:28:28 +0530

    add spinners

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react"; 
  
 import MainComponets from "./Components/MainComponets";
+import Spinner from "./Spinners";
   
 //  https://api.nasa.gov/planetary/apod?api_key=mrhh2R9IjDc8S1kU8oNN44UoHiqQtXqjkpbIwE5H
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    },  5000);
+  }, [ ])
+  
+
+   
   return (
      <> 
        <div   >
-        <MainComponets/>
-
+        
+       {loading ? <Spinner/> : <MainComponets/>}
       {/* //  <Routes>
  
       //   <Route path="/" element={<Navbar />}>
