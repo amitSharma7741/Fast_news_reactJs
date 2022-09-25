3d139c3c33cfa31891194ba8dc65e4c1bd9145ab
amitSharma7741 <183099@nith.ac.in>
2022-09-26 00:23:28 +0530

    add some css

--- src/Components/Pages/NoPage.js
+++ src/Components/Pages/NoPage.js
@@ -1,9 +1,32 @@
 import React from 'react'
-
 const NoPage = () => {
   return (
-    <div>NoPage</div>
+    <>
+      {/* make  responsive no page */}
+      <div style={{
+        height: "100vh",
+        width: "100%",
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+        paddingTop: "10px",
+        fontSize: "20px",
+        fontWeight: "bold"
+      }}>
+        <h1 style={{
+          fontSize: "100px",
+          fontWeight: "bold",
+          color: "white"
+        }}
+        >404</h1>
+        <h1 style={{
+          fontSize: "50px",
+          fontWeight: "bold",
+          color: "white"
+        }}
+        >Page Not Found</h1>
+      </div>
+    </>
   )
 }
-
 export default NoPage
