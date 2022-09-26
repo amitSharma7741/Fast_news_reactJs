c574d4838fc3e87e85c70729c8d986b9516f63fc
amitSharma7741 <183099@nith.ac.in>
2022-09-27 00:14:53 +0530

    add favicons and seo meta tags and remove favicons

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 import * as serviceWorker from "./serviceWorker";
-import Favicon from 'react-favicon'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-    <Favicon url='http://oflisback.github.io/react-favicon/img/github.ico' />
-    <App />
+      <App />
     </Router>
-  
+
   </React.StrictMode>
-); 
+);
 // node --trace-deprecation ...
 serviceWorker.unregister();
