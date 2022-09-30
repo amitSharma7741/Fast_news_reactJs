57876e4b1858d14fb7c11ed6a02e55f7437b7c11
amitSharma7741 <183099@nith.ac.in>
2022-09-30 14:35:53 +0530

    add url path

--- src/Components/Pages/Entertainement/EntertainementNavbar.js
+++ src/Components/Pages/Entertainement/EntertainementNavbar.js
@@ -1,8 +1,16 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { FaGreaterThan, FaHome } from "react-icons/fa";
 import "./Entertainement.css"
 const EntertainementNavbar = () => {
-   const [data, setData] = useState([]);
+    //  we need end path of url 
+    //  like https://blogs-data.onrender.com/bollywood/news
+    //  news is end path
+    const endPath = window.location.pathname.split("/").pop();
+    const [data, setData] = useState([]);
+    const [urlPath, setUrlPath] = useState(endPath);
+    // const [nextUrl , setNextUrl] = useState("");
+    // const [homeUrl , setHomeUrl] = useState("");
     const navData = [
         {
             id: 1,
@@ -35,28 +43,28 @@ const EntertainementNavbar = () => {
             url: "hollywood"
         },
         {
-            id:7,
-            value:"Box-Office",
-            url:"box-office"
+            id: 7,
+            value: "Box-Office",
+            url: "box-office"
         },
         {
-            id:8,
-            value:"Reviews",
-            url:"reviews"
+            id: 8,
+            value: "Reviews",
+            url: "reviews"
         },
         {
-            id:9,
-            value:"Style",
-            url:"style"
+            id: 9,
+            value: "Style",
+            url: "style"
         },
         {
-            id:10,
-            value:"Photos",
-            url:"photos"
+            id: 10,
+            value: "Photos",
+            url: "photos"
         }
     ]
-     
-    const url ="https://blogs-data.onrender.com/bollywood"
+
+    const url = "https://blogs-data.onrender.com/bollywood"
     const rightBox = async () => {
         try {
             const response = await fetch(url);
@@ -67,96 +75,131 @@ const EntertainementNavbar = () => {
         }
     }
 
-  useEffect(() => {
-    rightBox();
-  }, []);
- 
-    
+    useEffect(() => {
+        rightBox();
+    }, []);
+
+
     return (
 
         <>
             <div class="container">
-                <h1 className="text-center mt-3 animate-charcter" style={{ color: "black",
-              fontSize:"20px"
-            }}>Entertainement</h1>
-                  <  ul class="  "
-                  style={{
-                    overflow:"hidden",
-                    whiteSpace:"nowrap",
-                    textOverflow:"ellipsis", 
-                    listStyle:"none",
-                    backgroundColor:"#bcbca1"
+                <h1 className="text-center mt-3 animate-charcter" style={{
+                    color: "black",
+                    fontSize: "20px"
+                }}>Entertainement</h1>
+                <ul class="text-start"
+                    style={{
+                        overflow: "auto",
+                        whiteSpace: "nowrap",
+                        backgroundColor: "#bcbca1"
 
-                  }}
-                  >
+                    }} >
+                    <li style={{
+                        // float:"left",
+                        // textAlign:"center",
+                        textDecoration: "none",
+                        // listStyle:"none",
+                        padding: "14px 6px",
+                        color: "black",
+                        fontSize: "17px",
+                        fontWeight: "bold",
+                        cursor: "pointer",
+                        display: "inline-block"
+                    }} onclick = {(e) => setUrlPath("entertainement")}
+                    >
+                        <Link to="/entertainement" className="nav-link">
+                            <FaHome />
+                        </Link>
+                    </li>
 
-                        {navData.map((item) => {
+                    {navData.map((item) => {
 
-                            return (
-                                < li className="" key={item.id}
+                        return (
+                            <li className="" key={item.id}
                                 style={{
-                                     float:"left",
-                                    textAlign:"center",
-                                    textDecoration:"none",
-                                    listStyle:"none",
-                                    padding:"14px 6px",
-                                    color:"black",
-                                    fontSize:"17px",
-                                    fontWeight:"bold" ,
-                                    cursor:"pointer"
-                                  
-
-                                
-                                    
+                                    // float:"left",
+                                    // textAlign:"center",
+                                    textDecoration: "none",
+                                    // listStyle:"none",
+                                    padding: "14px 6px",
+                                    color: "black",
+                                    fontSize: "17px",
+                                    fontWeight: "bold",
+                                    cursor: "pointer",
+                                    display: "inline-block"
                                 }}
-                                >
-                                    <Link className= "" style = 
-                                    {{ 
-                                        color:"black"
-                                    }}
+                                onClick={() => setUrlPath(item.url)} >
+                                <Link style={{
+                                    color: "black"
+                                }}
                                     aria-current="page" to={`/entertainement/${item.url}`}>{item.value}</Link>
 
-                                </li>
-                            )
-                        })}
-                    </ ul>
-
+                            </li>
+                        )
+                    })}
+                </ ul>
+                <div className="text-start" style={{
+                    overflow: "auto",
+                    whiteSpace: "nowrap",
+                }}>
+                    <Link to={'/'} style={{ color: "red", display: "inline-block" }} >
+                        Home
+                    </Link>
+                    <div className="text-muted" style={{ display: "inline-block", margin: "0px 10px" }}>
+                        <FaGreaterThan />
+                    </div>
+                    <Link to={'/entertainement'} style={{ color: "red", display: "inline-block" }} >
+                        Entertainement
+                    </Link>
+                    {urlPath !== "entertainement" && (
+                        <>
+                           <div className="text-muted" style={{ display: "inline-block", margin: "0px 10px" }}>
+                                    <FaGreaterThan />
+                                </div>
+                                <p className="text-muted" style={{ display: "inline-block" }}>
+                                    {urlPath}
+                                </p>
+                        </>
+                    )}
+                </div>
                 <div className="row">
+
                     <div className="col-sm-8">
 
                         {<Outlet />}
                     </div>
-                    <div className="col-sm-4 mt-5">
+                    <div className="col-sm-4 mt-3">
 
-                         <div>
-                             {data.map((item) => {
+                        <div>
+                            {data.map((item) => {
                                 return (
                                     <>
-                                    <h2 className="text-center animate-charcter" key={item.head}
-                                    style={{
-                                        color:"black",
-                                        fontSize:"20px"
+                                        <h2 className="text-center animate-charcter" key={item.head}
+                                            style={{
+                                                color: "black",
+                                                fontSize: "20px"
 
-                                    }}
-                                    >{item.head} </h2>
-                                        <img src={item.imageUrl} className="w-100" alt={item.title} 
-                                        style={{
-                                            height:"250px"
+                                            }}
+                                        >{item.head} </h2>
+                                        <img src={item.imageUrl} className="w-100" alt={item.title}
+                                            style={{
+                                                height: "250px"
 
-                                        }}
+                                            }}
                                         />
 
-                                    <h5 className="mt-2"
-                                    style={{
-                                        color:"black",
-                                        fontSize:"18px",
-                                        textAlign:"justify"
-                                    }}
-                                    >{item.title.replace(/\s+/g, ' ').trim()}</h5>
-                                     </>   
+                                        <h5 className="mt-2"
+                                            style={{
+                                                color: "black",
+                                                fontSize: "18px",
+                                                textAlign: "justify"
+                                            }}
+                                        >{item.title.replace(/\s+/g, ' ').trim()}</h5>
+                                    </>
                                 )
-                             })}
-                         </div>
+                            })}
+                        </div>
 
                     </div>
                 </div>
