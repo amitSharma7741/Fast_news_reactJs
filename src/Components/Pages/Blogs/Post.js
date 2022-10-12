391521183221967be025b7fbaa9232ad1b04b53d
amitSharma7741 <183099@nith.ac.in>
2022-10-12 13:08:38 +0530

    link not defined

--- src/Components/Pages/Blogs/Post.js
+++ src/Components/Pages/Blogs/Post.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams , useNavigate } from "react-router-dom";
+import { useParams , useNavigate, Link } from "react-router-dom";
 // import usefetch from "../CustomHooks/UseFetchHook";
 import { FaArrowLeft } from 'react-icons/fa';
 import "./Post.css"
