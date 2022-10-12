c579257a65b9cbcbabf358b4430bd8313b63a643
amitSharma7741 <183099@nith.ac.in>
2022-10-12 11:59:47 +0530

    tag related blogs

--- src/Components/Pages/Blogs/TagPost.js
+++ src/Components/Pages/Blogs/TagPost.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { Link, Navigate } from 'react-router-dom';
+
+import { useNavigate, Link, useParams } from "react-router-dom";
+import ReactMarkdown from 'react-markdown'
 import Loader from '../../Loaders/Loader';
 // import { useParams , useNavigate } from "react-router-dom";
 
 const TagPost = () => {
 
     // const { tagId } = useParams(); // get the id from the url
-//   const navigate = useNavigate(); // navigate to another page
-
+    //   const navigate = useNavigate(); // navigate to another page
+    const { tagId } = useParams();
+    const navigate = useNavigate();
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    const url = "https://blogs-data.onrender.com/blog"
+    const url = `https://blogs-data.onrender.com/blog/${tagId}`;
     const datar = async () => {
         try {
             const res = await fetch(url);
@@ -31,9 +34,10 @@ const TagPost = () => {
 
     useEffect(() => {
         datar()
+        // eslint-disable-next-line
     }, [url]);
 
-    if (loading) { return ( <Loader /> ) } 
+    if (loading) { return (<Loader />) }
     if (error) return "Error!";
 
 
@@ -41,45 +45,73 @@ const TagPost = () => {
         <>
             <div className="container row row-cols-1 row-cols-md-3 g-4 ">
 
-                {data.map((i, product) => {
-                    return (
-                        <>
-                            <div className="col" key={product._id}>
-                                {/* if i less than 10 than place 0 like 0i */}
+                {data.map((product) => (
+                    <>
+                        <div className="col" key={product._id}>
+                            {/* if i less than 10 than place 0 like 0i */}
 
-                                <div class="paragraph grey" style={{
-                                    paddingTop: "0em",
-                                    paddingBottom: "1em",
-                                    color: "rgba(0, 0, 0, 0.4)"
-                                }}>0{i}</div>
-                                <div className="card h-100" onClick={() => {
-                                    Navigate(`/blog/${product.titleUrl}`);
-                                }} style={{
-                                    border: "0px"
-                                }}>
-                                    <img src={product.imageUrl} className="card-img-top" alt="..." style={{
-                                        height: "200px",
-                                        borderRadius: "1em"
+                            <div style={{
+                                paddingTop: "0em",
+                                paddingBottom: "1em",
+                                color: "rgba(0, 0, 0, 0.4)"
+                            }}>01</div>
+                            <div className="card h-100" onClick={() => {
+                                navigate(`/blog/${product.titleUrl}`);
+                            }} style={{
+                                border: "0px"
+                            }}>
+                                <img src={product.imageUrl} className="card-img-top" alt="..." style={{
+                                    height: "200px",
+                                    borderRadius: "1em"
+                                }} />
+                                <div className="card-body">
+                                    <Link to={`/blog/${product.titleUrl}`}>
+                                        <h5 className="card-title text-uppercase text-decoration-none " style={{
+                                            color: "black",
+                                            textAlign: "justify"
+                                        }}>{product.title.replace(/\s+/g, ' ').trim()}</h5>
+
+                                    </Link>
+                                    {/* <p className="card-text"> */}
+                                    <ReactMarkdown>
+                                        {/* display only 100 words */}
+                                        {product.description?.substring(0, 100)}
+                                    </ReactMarkdown>
+                                    {/* </p> */}
+                                    {/* product also have tags array add element with # */}
+                                    <div className="d-flex  justify-content-between ">
+                                        {product.tags?.map((tag) => (
+                                            <span style={{
+                                                fontSize: "0.8em"
+                                            }}
+                                                onClick={() => {
+                                                    navigate(`/blog/${tag}`);
+                                                }}
+                                            >#{tag}</span>
+                                        ))}
+
+                                    </div>
+                                    {/* add a horizontal line*/}
+                                    <hr className="mt-2" style={{
+                                        color: "rgba(0, 0, 0, 0.4)",
+                                        height: 1,
+                                        backgroundColor: "rgba(0, 0, 0, 0.4)"
+
+
                                     }} />
-                                    <div className="card-body">
-                                        <Link to={`/blog/${product.titleUrl}`}>
-                                            <h5 className="card-title text-uppercase text-decoration-none " style={{
-                                                color: "black"
-                                            }}>{product.title}</h5>
 
-                                        </Link>
-                                        {/* <p className="card-text"> */}
-                                        <ReactMarkdown>
-                                            {/* display only 100 words */}
-                                            {product.description.substring(0, 100)}
-                                        </ReactMarkdown>
-                                        {/* </p> */}
-                                    </div> 
+
+
                                 </div>
+                                {/* <div className="card-footer">
+    <small className="text-muted">{product.published}</small>
+    </div> */}
+
+
                             </div>
-                        </>
-                    );
-                })}
+                        </div>
+                    </>
+                ))}
 
             </div>
         </>
