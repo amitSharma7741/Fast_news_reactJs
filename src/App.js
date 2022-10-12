562206ed73ce0832a738d951dced1fa538c48c3d
amitSharma7741 <183099@nith.ac.in>
2022-10-12 11:57:57 +0530

    add all routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,51 @@
-import React, { useEffect, useState } from "react"; 
-import Loader from "./Components/Loaders/Loader";
- 
-import MainComponets from "./Components/MainComponets";
-// import Spinner from "./Spinners";
-  
-
-
-export default function App() {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-    },  4000);
-  }, [ ])
-  
-
-   
+import React from 'react' 
+import { Route, Routes } from 'react-router-dom'  
+import DeliveredComponents from './Components/DeliveredComponents'
+import Navbar from './Components/Navbar'
+import AppendPost from './Components/Pages/AppendPost/AppendPost'
+import Allblogs from './Components/Pages/Blogs/Allbogs'
+import Blog from './Components/Pages/Blogs/Blog'
+import Post from './Components/Pages/Blogs/Post'
+import TagPost from './Components/Pages/Blogs/TagPost'
+import Entertainement from './Components/Pages/Entertainement/Entertainement'
+import EntertainementNavbar from './Components/Pages/Entertainement/EntertainementNavbar'
+import EntertainePost from './Components/Pages/Entertainement/EntertainePost'
+import News from './Components/Pages/News/News'
+import NewsNavbar from './Components/Pages/News/NewsNavbar'
+import NewsPost from './Components/Pages/News/NewsPost'
+import NoPage from './Components/Pages/NoPage'
+import Testing from './Components/StartingPage/Testing'
+const App = () => {
   return (
-     <> 
-       <div   >
-        
-       {loading ? <Loader /> : <MainComponets/>}
-      
-       </div>
-        
-       </>
-  );
+    <>
+
+
+      <Routes>
+        <Route path="/" element={<Navbar />}> 
+          <Route index element={<DeliveredComponents />} />
+          <Route path="/about" element={<Testing />} />
+          <Route path="/entertainment" element={<EntertainementNavbar />} >
+            <Route index element={<Entertainement />} />
+            <Route path=":bolly" element={<EntertainePost />} />
+          </Route>
+          <Route path='/news' element = {<NewsNavbar/>} >
+            <Route index element = {<News/>}/>
+            <Route path=':newsId' element ={<NewsPost/>}/>
+          </Route>
+          <Route path="/write" element={<AppendPost />} />
+          <Route path="/blog" element={<Blog />} >
+            <Route path="" element={<Allblogs />} />
+            <Route path=":id" element={<Post />} />
+            <Route path=':tagId' element = {<TagPost /> } />
+          </Route>
+          {/* <Route path="/start" element={<StartingPage/> } /> */}
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+
+     
+    </>
+  )
 }
  
- 
+export default App
