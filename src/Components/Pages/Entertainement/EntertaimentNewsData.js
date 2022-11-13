43f1c7acae61a2b8b109478377b208cc7df4df75
Amit <183099@nith.ac.in>
2022-11-13 23:30:07 +0530

    sample data

---
+++ src/Components/Pages/Entertainement/EntertaimentNewsData.js
@@ -0,0 +1,143 @@
+export const EntertainmentNewsdata = 
+[
+    {
+      "title": "Bigg Boss 16: Nimrit Kaur Ahluwalia trolled for failing to explain 'open and shut case' despite being a qualified lawyer [Read Tweets]",
+      "link": "https://www.bollywoodlife.com/bigg-boss/bigg-boss-16-nimrit-kaur-ahluwalia-trolled-for-failing-to-explain-open-and-shut-case-despite-being-a-qualified-lawyer-read-tweets-2244048/",
+      "description": "Bigg Boss 16: Nimrit Kaur Ahluwalia trolled for failing to answer what is an open and shut case though she is a qualified lawyer on the show",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/nimrit-1.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 9:31 PM IST"
+    },
+    {
+      "title": "Rakesh Kumar prayer meet: Abhishek Bachchan, Jaya Bachchan, Shabana Azmi and other celebs pay their last respects [View Pics]",
+      "link": "https://www.bollywoodlife.com/photos/rakesh-kumar-prayer-meet-abhishek-bachchan-jaya-bachchan-shabana-azmi-and-other-celebs-pay-their-last-respects-view-pics-2244042/",
+      "description": "A prayer meet was held for late director Rakesh Kumar who passed away on November 10. Abhishek Bachchan, Jaya Bachchan, Shabana Azmi and more celebs paid their last respects to the ace director.",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/Abhishek-Jaya-Shabana.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 9:04 PM IST"
+    },
+    {
+      "title": "Ghum Hai Kisikey Pyaar Meiin: Sai snatches Saavi from Chavan Niwas and other high drama we can look forward to in coming week",
+      "link": "https://www.bollywoodlife.com/photos/ghum-hai-kisikey-pyaar-meiin-sai-snatches-saavi-from-chavan-niwas-and-other-high-drama-we-can-look-forward-to-in-coming-week-entertainment-news-2244008/",
+      "description": "Ghum Hai Kisikey Pyaar Meiin: In the coming week, we will see that Sai makes a dramatic entry and snatches Savi from Chavan House; Virat's DCP supports them",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/FotoJet-1.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 8:21 PM IST"
+    },
+    {
+      "title": "Yashoda Box Office: Samantha Ruth Prabhu movie fares better than Thank You starring Naga Chaitanya and Nagarjuna starrer The Ghost",
+      "link": "https://www.bollywoodlife.com/photos/yashoda-box-office-samantha-ruth-prabhu-movie-fares-better-than-nagarjuna-starrer-the-ghost-and-naga-chaitanya-romance-thank-you-entertainment-news-2243948/",
+      "description": "Yashoda Box Office: Samantha Ruth Prabhu thriller with backdrop of surrogacy fares better than Nagarjuna's The Ghost and Thank You of Naga Chaitanya with encouraging word of mouth and footfalls",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/MicrosoftTeams-image-34.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 7:21 PM IST"
+    },
+    {
+      "title": "BTS: SUGA aka Min Yoongi and Jimin bowl ARMY over with their nifty moves in the Run BTS Dance Practice [Watch Video]",
+      "link": "https://www.bollywoodlife.com/hollywood/bts-suga-aka-min-yoongi-and-jimin-bowl-army-over-with-their-nifty-moves-in-the-run-bts-dance-practice-watch-video-korean-entertainment-news-2243909/",
+      "description": "BTS: SUGA aka Min Yoongi and Park Jimin slay the dance floor as Bangtan TV releases the full Dance Practice of the chartbuster Run BTS [Watch Video]",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/MicrosoftTeams-image-26.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 6:00 PM IST"
+    },
+    {
+      "title": "Jaya Bachchan recalls changing sanitary pads behind bushes on outdoor shoots",
+      "link": "https://www.bollywoodlife.com/news-gossip/jaya-bachchan-recalls-changing-sanitary-pads-behind-bushes-on-outdoor-shoots-2243925/",
+      "description": "Veteran actress Jaya Bachchan recalled her embarassing experience with periods and how she used to change sanitary pads behind bushes while shooting outdoors.",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/Navya-Jaya-1.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 5:45 PM IST"
+    },
+    {
+      "title": "BTS: Kim Taehyung and Jennie Kim face nasty body-shaming comments as their fans fight it out on social media [Read Tweets]",
+      "link": "https://www.bollywoodlife.com/hollywood/bts-kim-taehyung-and-jennie-kim-face-nasty-body-shaming-comments-as-their-fans-fight-it-out-on-social-media-read-tweets-korean-entertainment-news-2243811/",
+      "description": "BTS: Kim Taehyung and Jennie Kim get body-shamed from rival fans; ARMYs tell people to focus on the music",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/10/BTS-Kim-Taehyung-Jennie-Blackpink-BLINK-BTS-ARMY.png?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 4:13 PM IST"
+    },
+    {
+      "title": "Sania Mirza-Shoaib Malik Divorce: Model Ayesha Omar faces the wrath of netizens; gets blamed for the split",
+      "link": "https://www.bollywoodlife.com/news-gossip/sania-mirza-shoaib-malik-divorce-model-ayesha-omar-faces-the-wrath-of-netizens-gets-blamed-for-the-split-entertainment-news-2243706/",
+      "description": "Sania Mirza-Shoaib Malik Divorce: Model Ayesha Omar faces relentless hate and trolling from netizens who hold her responsible for the split of the loved couple",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/MicrosoftTeams-image-25.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 2:11 PM IST"
+    },
+    {
+      "title": "Sania Mirza-Shoaib Malik Divorce: Lavish homes, swanky cars, brand endorsements; estranged couple' combined assets are over Rs 500 crores",
+      "link": "https://www.bollywoodlife.com/photos/sania-mirza-shoaib-malik-divorce-lavish-homes-swanky-cars-brand-endorsements-estranged-couple-combined-assets-are-over-rs-500-crores-entertainment-news-2243637/",
+      "description": "Sania Mirza-Shoaib Malik Divorce: From a lavish bungalow in Dubai to homes in Hyderabad, Karachi and fleet of cars, the estranged power couple knows how to live king size",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/MicrosoftTeams-image-18.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 12:45 PM IST"
+    },
+    {
+      "title": "Samantha Ruth Prabhu fan makes special request to Naga Chaitanya and Akkineni family as Yashoda star is diagnosed with myositis",
+      "link": "https://www.bollywoodlife.com/south-gossip/samantha-ruth-prabhu-fan-makes-special-request-to-naga-chaitanya-and-akkineni-family-as-yashoda-star-is-diagnosed-with-myositis-south-entertainment-news-2243577/",
+      "description": "Samantha Ruth Prabhu fan goes viral for making a special request to Naga Chaitanya and Nagarjuna to give moral support to the Yashoda actress",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/Samantha-2022-11-09T162730.323.png?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 11:48 AM IST"
+    },
+    {
+      "title": "Ranbir Kapoor-Alia Bhatt Vastu Home: Brahmastra duo's baby girl resides in this classic yet modern space; Rishi Kapoor's pic is UNMISSABLE",
+      "link": "https://www.bollywoodlife.com/photos/ranbir-kapoor-alia-bhatt-vastu-home-brahmastra-duos-baby-girl-resides-in-this-classic-yet-modern-space-rishi-kapoors-pic-is-unmissable-entertainment-news-2243492/",
+      "description": "Ranbir Kapoor and Alia Bhatt's Mumbai home is very stylish, classic yet modern. It has a bar, library, study room and loads of open spaces. Check out where Ranbir and Alia will raise their baby girl.",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/Ranbir-Kapoor-Alia-Bhatt-Vastu-home-pics1.png?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 10:51 AM IST"
+    },
+    {
+      "title": "Bigg Boss 16: Teary Priyanka Chahar Choudhary gets trolled for 'Balidaan' comment; visuals of Ankit Gupta consoling her will win your heart [View Pics]",
+      "link": "https://www.bollywoodlife.com/photos/bigg-boss-16-teary-priyanka-chahar-choudhary-gets-trolled-for-balidaan-comment-visuals-of-ankit-gupta-consoling-her-will-win-your-heart-view-pics-entertainment-news-2243543/",
+      "description": "Bigg Boss 16: Priyanka Chahar Choudhary trolled for Balidaan comment and selfish outlook, but these pics of Ankit Gupta consoling her will give you all the feels [View Pics]",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/pcc-and-ankitt.png?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 10:42 AM IST"
+    },
+    {
+      "title": "Bigg Boss 16: Sreejita De has the last laugh on Twitter as Tina Datta is exposed as 'insecure and dominating'; fans say, 'So True'",
+      "link": "https://www.bollywoodlife.com/bigg-boss/bigg-boss-16-sreejita-de-slams-tina-datta-as-insecure-and-dominating-on-twitter-fans-say-so-true-entertainment-news-2243446/",
+      "description": "Bigg Boss 16: Sreejita De took to Twitter to reiterate that Tina Datta is bossy after both Priyanka Chahar Choudhary and Sumbul Touqeer said the same thing",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/10/Bigg-Boss-16-Tina-Datta-Sreejita-De-Uttaran.png?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 9:29 AM IST"
+    },
+    {
+      "title": "It’s AWKWARD! Kriti Sanon, Urfi Javed and more celebs get caught in embarassing moments",
+      "link": "https://www.bollywoodlife.com/photos/its-awkward-kriti-sanon-urfi-javed-and-more-celebs-get-caught-in-embarassing-moments-2243392/",
+      "description": "This week, Kriti Sanon, Urfi Javed, Suniel Shetty, Mouni Roy, Rakul Preet Singh and Shefali Jariwala, were caught in embarassing moments.",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/Kriti-Urfi.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 8:00 AM IST"
+    },
+    {
+      "title": "Bipasha Basu and Karan Singh Grover's daughter born on Sankashti Chaturthi is a truly special child, reveals astro expert [Exclusive]",
+      "link": "https://www.bollywoodlife.com/news-gossip/bipasha-basu-and-karan-singh-grovers-daughter-born-on-sankashti-chaturthi-is-a-truly-special-child-reveals-astro-expert-exclusive-2243407/",
+      "description": "Bipasha Basu and Karan Singh Grover's daughter who arrived on Sankasthi Chaturthi is going to bless the couple's life. Read predictions by celeb astrologer Jagannath Guruji [Exclusive]",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/FotoJet.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 7:14 AM IST"
+    },
+    {
+      "title": "Trending Entertainment News Today: Reason why Sania Mirza, Shoaib Malik haven't broken silence on divorce; Bipasha Basu gives birth to a baby girl and more",
+      "link": "https://www.bollywoodlife.com/news-gossip/trending-entertainment-news-today-reason-why-sania-mirza-shoaib-malik-havent-broken-silence-on-divorce-bipasha-basu-gives-birth-to-a-baby-girl-and-more-2243381/",
+      "description": "Real reason behind Sania Mirza and Shoaib Malik's silence on their divorce has been revealed. Bipasha Basu and Karan Singh Grover have becomed proud parents to a baby girl, here are the top trending entertainment news today.",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/Sania-Bipasha.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 13, 2022 1:00 AM IST"
+    },
+    {
+      "title": "Bigg Boss 16: Gori Nagori blasts Sajid Khan, disappointed with Shiv Thakare; says he is playing a dirty game [Exclusive]",
+      "link": "https://www.bollywoodlife.com/bigg-boss/bigg-boss-16-gori-nagori-blasts-sajid-khan-disappointed-with-shiv-thakare-says-he-is-playing-a-dirty-game-exclusive-2243263/",
+      "description": "Bigg Boss 16: Gori Nagori in an EXCLUSIVE interview blasts Sajid Khan for making her wash his clothes; reveals her friendship with MC Stan is now kaput",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/FotoJet-2022-11-12T193906.623.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 12, 2022 9:50 PM IST"
+    },
+    {
+      "title": "BTS: Jin confirms he will be joining the Korean military before his birthday; ARMY heartbroken knowing he'll serve in a freezing snow-clad region",
+      "link": "https://www.bollywoodlife.com/hollywood/bts-jin-confirms-he-will-be-joining-the-korean-military-before-his-birthday-army-heartbroken-knowing-hell-serve-in-a-freezing-snow-clad-region-korean-entertainment-news-2243297/",
+      "description": "BTS: Jin confirms that he will be in the military's front line on his birthday; ARMY heartbroken knowing he will be serving in the freezing cold",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/jin-22.png?impolicy=Medium_Widthonly&w=303",
+      "published": "November 12, 2022 9:43 PM IST"
+    },
+    {
+      "title": "Hera Pheri 3: Akshay Kumar breaks silence on Kartik Aaryan replacing him as Raju; says, 'I was not satisfied...'",
+      "link": "https://www.bollywoodlife.com/news-gossip/hera-pheri-3-akshay-kumar-breaks-silence-on-kartik-aaryan-replacing-him-as-raju-says-i-was-not-satisfied-2243295/",
+      "description": "Fans were disappointed after Kartik Aaryan replaced Akshay Kumar as Raju in Hera Pheri 3. The Khiladi Kumar has broken his silence on why chose not to do the film.",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/Akshay-Kartik.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 12, 2022 9:09 PM IST"
+    },
+    {
+      "title": "Trending TV News Today: Siddhaanth Vir Surryavanshi funeral, Bigg Boss 16 contestant Ankit Gupta slammed by Kashmera Shah and more",
+      "link": "https://www.bollywoodlife.com/photos/trending-tv-news-today-siddhaanth-vir-surryavanshi-funeral-bigg-boss-16-contestant-ankit-gupta-slammed-by-kashmera-shah-and-more-entertainment-news-2243212/",
+      "description": "Trending TV News Today: Siddhaanth Vir Surryavanshi funeral, Kashmera Shah takes a dig at Ankit Gupta-Priyanka Chahar Choudhary relationship and so on",
+      "imageUrl": "https://st1.bollywoodlife.com/wp-content/uploads/2022/11/FotoJet-2022-11-12T202826.912.jpg?impolicy=Medium_Widthonly&w=303",
+      "published": "November 12, 2022 8:56 PM IST"
+    }
+  ]
