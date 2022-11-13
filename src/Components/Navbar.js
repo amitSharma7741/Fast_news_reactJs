b94923ac661ea82b02c0571bb1e6376928756102
Amit <183099@nith.ac.in>
2022-11-13 23:30:41 +0530

    navbar hover effect

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,14 +2,48 @@ import "./Navbar.css"
 import React, { useEffect, useState } from 'react'
 import { FaAngleUp } from 'react-icons/fa';
 import { FaAngleDown } from 'react-icons/fa';
-import {Link,Outlet} from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 import Footer from "./Pages/Footer/Footer";
 
 //  https://api.nasa.gov/planetary/apod?api_key=mrhh2R9IjDc8S1kU8oNN44UoHiqQtXqjkpbIwE5H
 const Navbar = () => {
 
   const [dim, setDim] = useState([]);
-  
+
+  const navData = [
+    {
+      id:1,
+      navLink : "Home",
+      url:"/"
+    },
+    {
+      id:2,
+      navLink:"Blog",
+      url:"/blog"
+    },
+    {
+      id:3,
+      navLink:"News",
+      url:"/news"
+    },
+    {
+      id:4,
+      navLink:"Entertainment",
+      url:"/entertainment"
+    },
+    {
+      id:5,
+      navLink:"Write",
+      url:"/write"
+    }
+  ]
+  /* <li className="nav-item" onClick={classChange}>
+                  <Link className="nav-link" to="/blog">
+                    Blog
+                  </Link>
+                </li> */
+   
+
   const url = "https://api.coincap.io/v2/assets/";
   const dataChange = async () => {
     const resul = await fetch(url);
@@ -21,9 +55,9 @@ const Navbar = () => {
   }, [url]);
 
 
- const classChange = () => {
-  document.querySelector(".navbar-toggler").click();
- }
+  const classChange = () => {
+    document.querySelector(".navbar-toggler").click();
+  }
 
 
   return (
@@ -58,7 +92,10 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
             <a className="navbar-brand" href="/">
-              NEWSMON
+              <b>
+                NEWSMON
+              </b>
+
             </a>
             <button
               className="navbar-toggler"
@@ -73,35 +110,67 @@ const Navbar = () => {
             </button>
             <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
               <ul className="navbar-nav  mb-2 mb-lg-0">
-                <li className="nav-item"
-                // after onclick navbar toggle close
-                onClick= {classChange}
+                
+                {navData.map((elem)=>{
+                  return (
+                    <>
+                    <li className="nav-item"
+                    key={elem.id}
+                  // after onclick navbar toggle close
+                  onClick={classChange}
+                  onMouseOver = {(e)=>{
+                    e.target.style.color = "white";
+                    e.target.style.backgroundColor = "black";
+                  }}
+                  onMouseOut = {(e)=>{
+                    e.target.style.color = "black";
+                    e.target.style.backgroundColor = ""
+                  }}
+                >
+                  <Link className="nav-link active" aria-current="page" to = {elem.url}>
+                     {elem.navLink}
+                  </Link>
+                </li>
+                    </>
+                  )
+                })}
+               {/*  <li className="nav-item"
+                  // after onclick navbar toggle close
+                  onClick={classChange}
+                  onMouseOver = {(e)=>{
+                    e.target.style.color = "white";
+                    e.target.style.backgroundColor = "black";
+                  }}
+                  onMouseOut = {(e)=>{
+                    e.target.style.color = "black";
+                    e.target.style.backgroundColor = ""
+                  }}
                 >
                   <Link className="nav-link active" aria-current="page" to="/">
                     Home
                   </Link>
                 </li>
-                <li className="nav-item" onClick= {classChange}>
+                <li className="nav-item" onClick={classChange}>
                   <Link className="nav-link" to="/blog">
                     Blog
                   </Link>
                 </li>
-                <li className="nav-item" onClick= {classChange}>
+                <li className="nav-item" onClick={classChange}>
                   <Link className="nav-link" to="/news">
-                     News
+                    News
                   </Link>
                 </li>
-                <li className="nav-item" onClick= {classChange}>
+                <li className="nav-item" onClick={classChange}>
                   <Link className="nav-link" to="/entertainment">
                     Entertainment
                   </Link>
                 </li>
-                <li className="nav-item" onClick= {classChange}>
+                <li className="nav-item" onClick={classChange}>
                   <Link className="nav-link" to="/write">
                     write
                   </Link>
-                </li>
-               {/*  <li className="nav-item" onClick= {classChange}> 
+                </li> */}
+                {/*  <li className="nav-item" onClick= {classChange}> 
                   <Link className="  nav-link" to="/about"  >
                     About Us
                   </Link>
@@ -170,13 +239,13 @@ const Navbar = () => {
         </nav>
       </div>
       <div style={{
-           minHeight:"100vh"
+        minHeight: "100vh"
       }}>
         <Outlet />
       </div>
-       <div>
+      <div>
         <Footer />
-       </div>
+      </div>
     </>
   )
 }
