9c750cbb2278dfea0104b75513f6e561615b1378
Amit <183099@nith.ac.in>
2022-12-01 21:34:54 +0530

    add email js

--- src/Components/Pages/Footer/Footer.js
+++ src/Components/Pages/Footer/Footer.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
 import { FaFacebook, FaGithub, FaTwitter, FaLinkedin  } from "react-icons/fa";
 const Footer = () => {
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    //   YOUR_SERVICE_ID
+    emailjs.sendForm('service_7tsmkdj', 'template_pryiqsj', form.current, '4ZifXAKalESRAW5qN')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    e.target.reset(); 
+};
   return (
     <>
 
@@ -121,10 +135,11 @@ const Footer = () => {
             <div className="col-lg-3 col-md-6 mb-4 mb-lg-0">
               <h5 className="text-uppercase mb-4">Sign up to our newsletter</h5>
               <div className="form-outline form-white mb-4">
-                <input type="email" id="form5Example2" className="form-control" />
+                <input type="email" id="form5Example2" ref={form}className="form-control" />
                 <label className="form-label" htmlFor="form5Example2">Email address</label>
               </div>
-              <button type="submit" className="btn btn-primary btn-outline-white btn-block">Subscribe</button>
+              <button type="submit" className="btn btn-primary btn-outline-white btn-block"  onclick = {sendEmail}
+              >Subscribe</button>
             </div>
             {/*Grid column*/}
           </div>
