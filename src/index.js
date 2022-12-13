910eac6c23e90a4704e785bcfc2d0e584188006d
Amit <183099@nith.ac.in>
2022-12-13 20:37:30 +0530

    add react ga for analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 import * as serviceWorker from "./serviceWorker";
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-250993129-2');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
